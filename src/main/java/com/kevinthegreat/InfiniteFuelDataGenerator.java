12d27c5590cf7132e67107959e71e9e8e15ad9fc
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2024-03-23 21:05:36 -0400

    Mod Setup

---
+++ src/main/java/com/kevinthegreat/InfiniteFuelDataGenerator.java
@@ -0,0 +1,11 @@
+package com.kevinthegreat;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class InfiniteFuelDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
